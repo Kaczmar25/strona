ed7bd0ecddf1a52ceff7f0b6d26f8746a9e2723d
Jakub Kaczmarek <jkaczmarek6@edu.cdv.pl>
2023-03-21 16:15:37 +0100

    update

--- 3_db/1_db.php
+++ 3_db/1_db.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Użytkownicy</title>
 </head>
 <body>
-    
+    <h4>Użytkownicy</h4>
+    <?php
+        require_once "../scripts/connect.php";
+        //echo $conn->connect_errno;
+        $sql = "SELECT * FROM `users`;";
+        $result = $conn->query($sql);
+        //$user = $result->fetch_all();
+        //$user = $result->fetch_assoc();
+        //echo $user["firstName"]."<hr>";
+        //print_r($user);
+        while($user = $result->fetch_assoc()){
+            $year = substr($user["birthday"], 0 , 4);
+            echo <<< USERS
+                Imię i nazwisko: $user[firstName] $user[lastName]<br>
+                Data urodzenia: $user[birthday]<br>
+                Rok urodzenia: $year
+                <hr>
+            USERS;
+        }
+    ?>
 </body>
 </html>
